7e039839721a2df7e5fcbe3d83f659078b5a433c
Tony Chan <tony.chan@vonage.com>
2026-07-29 12:27:10 -0700

    initial

---
+++ README.md
@@ -0,0 +1,143 @@
+# Sample application using Vonage Voice API to connect Voice Calls into conference bridges
+
+You may use this Voice API application to connect voice calls into conference bridges and stream audio for each participant to/from AI Engines using one of the Connectors listed in the Set up section.
+ 
+Voice calls may be:</br>
+inbound/outbound,</br>
+PSTN calls (cell phones, landline phones, fixed phones),</br>
+SIP calls with [SIP endpoints](https://developer.vonage.com/en/voice/voice-api/concepts/endpoints#session-initiation-protocol-sip) or [Programmable SIP](https://developer.vonage.com/en/voice/voice-api/concepts/programmable-sip),</br>
+[WebRTC](https://developer.vonage.com/en/vonage-client-sdk/overview) calls (iOS/Android/Web Javascript clients).</br>
+
+
+## About this sample Voice API application
+
+This application connects voice calls to a Connector server by using the [WebSockets feature](https://developer.vonage.com/en/voice/voice-api/concepts/websockets) of Vonage Voice API.</br>
+
+When a voice call is established, this Voice API application triggers a WebSocket connection from Vonage platform to the Connector server which streams audio in one or both directions between the voice call and the AI engines. 
+
+Instead of using this sample Voice API application, you may use your own existing Voice API application to establish WebSockets with the Connector server to connect your managed voice calls with the AI engines.
+
+Your new or existing Voice API application may be written with any programming language using [server SDKs](https://developer.vonage.com/en/tools) or with direct [REST API](https://developer.vonage.com/en/api/voice) calls.
+
+You may instead have your Vonage [Video API WebRTC Clients](https://developer.vonage.com/en/tools) establish sessions with AI engines through the Video API's [Audio Connector](https://developer.vonage.com/en/video/guides/audio-connector) and the peer Connector server as listed in the next section, in which case this Voice API application is not needed.
+
+## Set up
+
+### Set up the sample Connector server - Host server public hostname and port
+
+First set up the Connector server (aka middleware server) from one of the following repositories</br>
+
+Deepgram Voice Agent</br>
+ https://github.com/nexmo-se/vonage-deepgram-voice-agent,</br></br>
+Deepgram ASR - OpenAI LLM - ElevenLabs TTS</br>
+https://github.com/nexmo-se/dg-oai-11l-connector,</br></br>
+ElevenLabs Agent</br>
+https://github.com/nexmo-se/elevenlabs-agent-ws-connector,</br></br>
+Deepgram ASR</br>
+https://github.com/nexmo-se/deepgram-connector,</br></br>
+OpenAI STS Realtime API</br>
+https://github.com/nexmo-se/openai-realtime-connector,</br></br>
+Hume STS EVI + Immersitech Noise Cancellation / Voice Isolation</br>
+https://github.com/nexmo-se/humeai-connector,</br></br>
+
+https://github.com/nexmo-se/websocket-server-variant-3.</br> 
+
+Default local (not public!) of any one of the Connector servers `port` is: 6000.
+
+If you plan to test using a `Local deployment`, you may use ngrok (an Internet tunneling service) for both<br>
+this Voice API application<br>
+and the Connector application<br>
+with [multiple ngrok tunnels](https://ngrok.com/docs/agent/config/v2/#tunnel-configurations).
+
+To do that, [install ngrok](https://ngrok.com/downloads).<br>
+Log in or sign up with [ngrok](https://ngrok.com/),<br>
+from the ngrok web UI menu, follow the **Setup and Installation** guide.
+
+Set up two tunnels,<br>
+one to forward to the local port 6000 (as the Connector application will be listening on port 6000),<br>
+the other one to the local port 8000 for this Voice API application,<br>
+see this [sample yaml configuration file](https://ngrok.com/docs/agent/config/v2/#define-two-tunnels-named-httpbin-and-demo), but it needs port 6000 and 8000 as actual values,<br>
+depending if you have a paid ngrok account or not, you may or may not be able to set (static) domain names.
+
+Start ngrok to start both tunnels that forward to local ports 6000 and 8000, e.g.<br>
+`ngrok start httpbin demo`
+
+please take note of the ngrok Enpoint URL that forwards to local port 6000 as it will be needed here for this Voice API application environment variable as **`PROCESSOR_SERVER`** in one of the next sections, that URL looks like:<br>
+`xxxxxxxx.ngrok.xxx` (for ngrok),<br>
+or `myserver.mycompany.com:32000` (public host name and port of your Connector application server)<br>
+no `port` is necessary with ngrok as public host name,<br>
+that host name to specify must not have leading protocol text such as `https://`, `wss://`, nor trailing `/`.
+
+### Set up your Vonage Voice API application credentials and phone number
+
+[Log in to your](https://ui.idp.vonage.com/ui/auth/login) or [sign up for a](https://ui.idp.vonage.com/ui/auth/registration) Vonage APIs account.
+
+Go to [Your applications](https://dashboard.vonage.com/applications), access an existing application or [+ Create a new application](https://dashboard.vonage.com/applications/new).
+
+Under Capabilities section (click on [Edit] if you do not see this section):
+
+**Enable** Voice
+- Under Answer URL, leave HTTP GET, and enter</br>
+https://\<host\>:\<port\>/answer</br>
+(replace \<host\> and \<port\> with the public host name and if necessary public port of the server where this sample application is running)</br>
+- Under Event URL, **select** HTTP POST, and enter</br>
+https://\<host\>:\<port\>/event</br>
+(replace \<host\> and \<port\> with the public host name and if necessary public port of the server where this sample application is running)</br>
+Note: If you are using ngrok for this sample application, the answer URL and event URL look like:</br>
+https://yyyyyyyy.ngrok.xxx/answer</br>
+https://yyyyyyyy.ngrok.xxx/event</br> 	
+- Click on [Generate public and private key] if you did not yet create or want new ones, save the private key file in this application folder as .private.key (leading dot in the file name).</br>
+
+- Make sure a value is selected in the dropdown under **Region**</br>
+_Note: There is no need to set a region in the application code itself_
+
+- Click on [Generate new application] if you've just created the application.</br></br>
+
+**IMPORTANT**: If you already have an existing application and just changed some parameter values including created a new public and private key set, do not forget to click on [Save changes] at the bottom of the screen.</br></br>
+
+- Link a phone number to this application if none has been linked to the application.</br>
+
+Please take note of your **application ID** and the **linked phone number** (as they are needed in the very next section).
+
+For the next steps, you will need:</br>
+- Your [Vonage API key](https://dashboard.nexmo.com/settings) (as **`API_KEY`**)</br>
+- Your [Vonage API secret](https://dashboard.nexmo.com/settings), not signature secret, (as **`API_SECRET`**)</br>
+- Your `application ID` (as **`APP_ID`**),</br>
+- The **`phone number linked`** to your application (as **`SERVICE_PHONE_NUMBER`**), your phone will **call that number**.</br>
+
+### Local deployment
+
+Copy or rename .env-example to .env<br>
+Update parameters in .env file<br>
+Have Node.js installed on your system, this application has been tested with Node.js version 22.16<br>
+
+Install node modules with the command:<br>
+ ```bash
+npm install
+```
+
+Launch the server application with either of the following commands:<br>
+```bash
+node simple-conference-call
+```
+or
+```bash
+node test-conf-call
+```
+
+The second server application does not need the participant to enter a conference number as it uses a default conference number just for tests. 
+
+Default local (not public!) `port` of either server application is: 8000.
+
+### How to call in
+
+#### Inbound calling
+
+Call the **`phone number linked`** to your application, enter a conference number if requested, each participant will get connected to the AI engine(s).
+
+#### Outbound calling
+
+Will be added to the code later
+
+
+
---
+++ test-conf-call.js
@@ -0,0 +1,531 @@
+'use strict'
+
+//-------------
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const fs = require('fs');
+
+const axios = require('axios');
+
+//-- CORS - update as needed for your environment -
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+
+//--
+
+app.use(bodyParser.json());
+
+//--- Optional - For VCR (Vonage Cloud Runtime, aka Neru) installation ----
+
+const neruHost = process.env.NERU_HOST;
+// console.log('neruHost:', neruHost);
+
+//------------------------------
+
+const appId = process.env.APP_ID;
+const serviceNumber = process.env.SERVICE_NUMBER;
+const apiRegion = process.env.API_REGION;
+const dc = apiRegion.substring(4, 8);
+
+// ------------------
+
+console.log("Service phone number:", serviceNumber);
+
+//-------------------
+
+const { Auth } = require('@vonage/auth');
+
+const credentials = new Auth({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+  applicationId: appId,
+  privateKey: './.private.key'
+});
+
+// const options = {
+//   apiHost: `https://${apiRegion}`
+// };
+
+//- test
+// const options = {
+//   apiHost: 'https://api-us.vonage.com'
+// };
+
+const { Vonage } = require('@vonage/server-sdk');
+
+// const vonage = new Vonage(credentials, options);
+const vonage = new Vonage(credentials);
+
+const privateKey = fs.readFileSync('./.private.key');
+
+const { tokenGenerate } = require('@vonage/jwt');
+
+//-- List of allowable client SDK (WebRTC client) users --
+
+const clientList = process.env.CLIENT_SDK_USERS.toLowerCase().split(/\s*,+\s*/);
+
+let clients = new Set();
+
+for (let client in clientList){
+  clients.add(clientList[client]);
+};
+
+console.log('List of allowable clients:', clients);
+
+// create IVR voice prompts for PSTN incoming calls, the following example assumes
+// you did specify more than 1 and up to 9 allowable client SDK user names (in .env file)
+
+// let ivrPrompt = "Welcome to our company. ";
+
+// let index = 0;
+// for (let client of clients) {
+//   index++;
+//   ivrPrompt = ivrPrompt + `To speak to ${client}, press ${index}. `;
+// };
+
+const clientsArray = Array.from(clients);
+
+// no IVR for direct in-app to in-app calls (WebRTC client to WebRTC client)
+
+//---- Connector server ----
+const connectorServer = process.env.PROCESSOR_SERVER;
+
+//---- Default language code ----
+const sttLanguageCode = process.env.STT_LANGUAGE_CODE
+
+//---- Call leg info tracking ----
+
+let callTracking = {}; // dictionary
+
+function addInfoToCallTracking(uuid) {
+  callTracking[uuid] = {};
+  callTracking[uuid]["confNumber"] = null;  // conference bridge number
+  callTracking[uuid]["peerWsUuid"] = null;  // associated WebSocket leg uuid
+  callTracking[uuid]["convUuid"] = null;
+  // callTracking[uuid]["startTime"] = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss:SSS'); // server local time
+  // callTracking[uuid]["startTime"] = moment.utc(Date.now()).format('YYYY-MM-DD HH:mm:ss:SSS'); // UTC time
+}
+
+function deleteFromCallTracking(uuid) {
+  delete callTracking[uuid];
+}
+
+//==========================================================
+
+app.get('/answer', (req, res) => {
+
+  addInfoToCallTracking(req.query.uuid);  // create info set for this leg
+
+  const hostName = req.hostname;
+
+  const  nccoResponse = [
+        {
+          "action": "talk",
+          "text": "Your call is now connected to the conference bridge number " + "4 5 6 7 8 9",
+          "language": "en-US",
+          "style": 0
+        },
+        {
+          "action": "conversation",
+          "name": "conf_" + "456789",
+          "startOnEnter": true
+        }
+      ];  
+
+  res.status(200).json(nccoResponse);
+
+});
+
+//--------
+
+app.post('/event', (req, res) => {
+
+  const uuid = req.body.uuid;
+
+  const hostName = req.hostname;
+
+  if (req.body.type == "transfer") {
+
+    // WebSocket URI to the connector server
+    const wsUri = 'wss://' + connectorServer + '/socket?peer_uuid=' + uuid + '&language_code=' + sttLanguageCode + '&webhook_url=https://' + hostName + '/results'; 
+
+    console.log('>>> WebSocket URI:', wsUri);
+
+    // setTimeout( () => {  // for tests only
+
+      // create corresponding websocket
+      vonage.voice.createOutboundCall({
+        to: [{
+          type: 'websocket',
+          uri: wsUri,
+          'content-type': 'audio/l16;rate=16000',  // NEVER change the content-type parameter argument
+          headers: {}
+        }],
+        from: {
+          type: 'phone',
+          number: serviceNumber // cannot use a longer than 15-digit string (e.g. not call_uuid)
+        },
+        answer_url: ['https://' + hostName + '/ws_answer?peer_uuid=' + uuid + '&conference_number=' + '456789'],
+        event_method: "GET",
+        event_url: ['https://' + hostName + '/ws_event?peer_uuid=' + uuid + '&conference_number=' + '456789'],
+        event_method: "POST"
+        // ncco: [
+        //   {
+        //     "action": "conversation",
+        //     "name": `conf_${conferenceNumber}`,
+        //     "startOnEnter": true,
+        //     "canHear": [uuid] // this WebSocket listens only to this very call leg
+        //   }
+        // ]
+      })
+        .then(res => {
+
+          callTracking[uuid]["wsUuid"] = res.uuid;  // store peer WebSocet leg uuid for original call info set
+          
+          addInfoToCallTracking(res.uuid);  // WebSocket uuid
+          callTracking[res.uuid]["convUuid"] = res.conversation_uuid; // Initial Conv uuid of this WebSocket leg
+          
+          console.log(">>> outgoing WebSocket call status:", res);
+        })
+        .catch(err => console.error(">>> outgoing WebSocket call error:", err));
+    
+    // }, 300)    
+
+  }
+
+  //--
+
+  if (req.body.status == "completed") {
+
+    // terminate peer WebSocket leg
+    const wsUuid = callTracking[uuid]?.["wsUuid"] ?? null;
+
+    // debug
+    console.log('>>> wsUuid:', wsUuid);
+
+    if (wsUuid) {
+      vonage.voice.hangupCall(wsUuid)
+        .then(res => console.log('>>> WebSocket ' + wsUuid + ' terminated'))
+        .catch(err => {
+          console.error('>>> WebSocket ' + wsUuid + ' tear down error', err)
+          // you may see error 400 bad request if this WebSocket leg has already terminated, that's not a problem
+        });
+    }
+
+    //---
+
+    setTimeout ( () => {
+
+      deleteFromCallTracking(uuid); // info set no longer needed
+
+    }, 5000)
+
+  }
+
+  //----
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+app.post('/inappevent', (req, res) => {
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+app.get('/ws_answer', (req, res) => {
+
+  // setTimeout( () => { // testing delaying response
+
+    const nccoResponse = [
+      {
+        "action": "conversation",
+        "name": "conf_" + "456789",
+        "startOnEnter": true,
+        "canHear": [req.query.peer_uuid] // this WebSocket listens only to the peer call leg
+      }
+    ];
+
+    res.status(200).json(nccoResponse);
+
+  // }, 300)  
+  
+});
+
+//--------
+
+app.post('/ws_event', (req, res) => {
+
+  const uuid = req.body.uuid;
+
+  //--
+
+  // if (req.body.type == "transfer") {
+
+  //   if (req.body.conversation_uuid_from == callTracking[uuid]["convUuid"]) {  // is it the first transfer event for this WebSocket leg?
+
+  //     // debug
+  //     console.log(">>> This is the 1st transfer to named conference");
+  //     console.log(">>> conference name:", "conf_" + req.query.conference_number);
+  //     console.log(">>> canhear:", req.query.peer_uuid);
+
+  //     const ncco =
+  //       [
+  //         {
+  //           "action": "conversation",
+  //           "name": "conf_" + "456789", // put back in same named conference
+  //           "canHear": [req.query.peer_uuid],
+  //           "startOnEnter": true
+  //         }
+  //       ];
+         
+  //     vonage.voice.transferCallWithNCCO(uuid, ncco)
+  //     .then(res => console.log(`>>> WebSocket ${uuid} audio controls updated`))
+  //     .catch(err => console.error(`>>> Updating WebSocket ${uuid} audio controls error:`, err))
+
+  //   } else {
+
+  //     // debug
+  //     console.log(">>> This is the subsequent transfer to named conference");
+
+  //   }
+
+  // }  
+
+  //--
+
+  if (req.body.status == "completed") {
+
+    setTimeout ( () => {
+      deleteFromCallTracking(uuid); // info set no longer needed
+    }, 5000)
+
+  }  
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+app.post('/results', (req, res) => {
+
+  console.log(req.body);
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+// app.post('/rtc', (req, res) => {
+
+//   res.status(200).send('Ok');
+  
+// });
+
+//=== Services for the WebRTC client (Vonage client SDK) ===============
+
+app.post('/login', async (req, res) => {
+
+    const user = req.body.user; // web page should have already made the name to lower case
+
+    // check if user is in the list of allowable users
+    if (!clients.has(user)) {
+      return res.status(401).json({ name: user, message: ">>> Unknown user" });
+    }
+
+    console.log("Creating user: " + user);
+    
+    // either get or create this user (if not yet existing)
+    const userId = await getUser(user);
+    
+    // console.log("Generating JWT for user: " + user);
+    const jwt = await generateJWT(user);
+        
+    return res.status(200).json({ name: user, userId: userId, token: jwt, dc: dc, phone: serviceNumber });
+})
+
+//--------
+
+async function getUser(name) {
+    
+  const accessToken = tokenGenerate(appId, privateKey, {});
+  
+  return new Promise(async (resolve, reject) => {
+    
+    let results;
+    
+    try {
+      results = await axios.get('https://api.nexmo.com/v0.3/users?name=' + name,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        });
+
+      //-- debug
+      // console.log(">>> results.data:", results.data);
+      // console.log("User Retrieval results: ", results.data._embedded.users[0].id);
+      
+      // If user already exists, just use it!
+      resolve(results.data._embedded.users[0].id);
+      return;
+    } 
+    catch (err) {
+
+        console.log(">>> err.response:", err.response);
+        // console.log("User retrieval error: ", err.response.data)
+    }
+    
+    // Here - user does NOT exist, create it
+    try {
+        let body = {
+            name: name,
+            display_name: name
+        }
+        results = await axios.post('https://api.nexmo.com/v0.3/users', body,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accessToken
+                }
+            });
+        
+        // debug
+        // console.log("User creation results: ", results.data);
+        
+        // New user created, pass back the id
+        resolve(results.data.id);
+        
+        return;
+    } 
+    catch (err) {
+        console.log(">>> User creation error:", err);
+        console.log("User creation error: ", err.response?.statusText)
+        
+        resolve(null);
+    }
+  })
+
+}
+
+//--------
+
+app.post('/logout', async (req, res) => {
+    
+    let user = req.body.user;
+    let session = req.body.session;
+    
+    // console.log("Deleting session: " + session);
+    await delSession(session);
+
+    return res.status(200).end();
+})
+
+//--------
+
+async function generateJWT(sub) {
+    
+    // Generate a JWT with the appropriate ACL
+    let jwtExpiration = Math.round(new Date().getTime() / 1000) + 2592000; //30 days
+    
+    const aclPaths = {
+        "paths": {
+            "/*/users/**": {},
+            "/*/conversations/**": {},
+            "/*/sessions/**": {},
+            "/*/devices/**": {},
+            "/*/image/**": {},
+            "/*/media/**": {},
+            "/*/applications/**": {},
+            "/*/push/**": {},
+            "/*/knocking/**": {},
+            "/*/legs/**": {}
+        }
+    }
+    let claims = {
+        exp: jwtExpiration,
+        //ttl: 86400,
+        acl: aclPaths,
+    }
+    
+    // ONLY Client JWTs use a "sub", so don't add one if it is already passed in
+    if (sub != null) {
+        claims.sub = sub
+    }
+    
+    // debug
+    // console.log(appId, privateKey, claims);
+    
+    const jwt = tokenGenerate(appId, privateKey, claims)
+    
+    // console.log("Jwt: ", jwt)
+    
+    return (jwt);
+}
+
+//--------
+
+async function delSession(session) {
+
+  const accessToken = tokenGenerate(appId, privateKey, {});
+  
+  return new Promise(async (resolve, reject) => {
+
+    let results;
+
+    try {
+      results = await axios.delete('https://api.nexmo.com/v0.3/sessions/' + session,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+          }
+        });
+      // console.log("User session deletion results: ", results.data);
+      resolve(results.data);
+      return;
+    } 
+    catch (err) {
+      console.log("User session deletion error: ", err)
+    }
+  })
+
+}
+
+//--------------- for VCR ----------------
+
+app.get('/_/health', async (req, res) => {
+   
+  res.status(200).send('Ok');
+
+});
+
+//========== Static HTTP server ===========
+
+app.use ('/', express.static(__dirname + '/public')); // static web server
+
+//=========================================
+
+const port = process.env.NERU_APP_PORT || process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Application listening on port ${port}`));
+
+//------------
